7b4a33775833de8be88b168a932897d293265f7c
stron <stron@DESKTOP-91PL7PD>
2022-10-30 06:34:37 +0300

    Добавьте файлы проекта.

---
+++ TrainingWPF/Pages/AdminPage2.xaml.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TrainingWPF.ModelDB;
+
+namespace TrainingWPF.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminPage2.xaml
+    /// </summary>
+    public partial class AdminPage2 : Page
+    {
+        public AdminPage2()
+        {
+            InitializeComponent();
+            dg.ItemsSource = DataBase.tbE.Users.ToList();
+           
+
+
+        }
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+            dg.Visibility = Visibility.Visible;
+            tbShowUser.Visibility = Visibility.Collapsed;
+            tbSleepUser.Visibility = Visibility.Visible; 
+
+        }
+
+        private void tbSleepUser_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            dg.Visibility = Visibility.Collapsed;
+            tbShowUser.Visibility = Visibility.Visible;
+            tbSleepUser.Visibility = Visibility.Collapsed;
+        }
+
+       
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            SortCombobox.SelectedIndex = -1;
+           
+            cmbGender.SelectedIndex = -1;
+
+            tbFiltres.Clear();
+            dg.ItemsSource = DataBase.tbE.Users.ToList();
+
+
+        }
+
+        private void SortedFiltres()
+        {
+            List<Users> users = new List<Users>();
+            if (SortCombobox.SelectedItem != null)
+            {
+                ComboBoxItem comboBoxItem = (ComboBoxItem)SortCombobox.SelectedItem;
+                switch (comboBoxItem.Content)
+                {
+                    case "По возрастанию":
+                        {
+                            dg.ItemsSource = DataBase.tbE.Users.OrderBy(x => x.Surname).ToList();
+                            break;
+                        }
+                    case "По убыванию":
+                        {
+                            dg.ItemsSource = DataBase.tbE.Users.OrderByDescending(x => x.Surname).ToList();
+                            break;
+                        }
+                }
+
+            }
+         
+
+
+        }
+
+        private void SortedGender()
+        {
+           
+            if (cmbGender.SelectedItem != null)
+            {
+                ComboBoxItem comboBoxItem = (ComboBoxItem)cmbGender.SelectedItem;
+                switch (comboBoxItem.Content)
+                {
+                    case "Мужской":
+                        {
+                            dg.ItemsSource = DataBase.tbE.Users.OrderBy(x => x.GenderTable.IdGender == 1 ).ToList();
+                            break;
+                        }
+                    case "Женский":
+                        {
+                            dg.ItemsSource = DataBase.tbE.Users.OrderBy(x => x.GenderTable.IdGender == 2).ToList();
+                            break;
+                        }
+                }
+
+            }
+            dg.ItemsSource = DataBase.tbE.Users.ToList();
+
+
+        }
+
+        private void cmbDesc_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            dg.ItemsSource = DataBase.tbE.Users.OrderByDescending(x => x.Surname).ToList();
+        }
+
+        private void SortCombobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SortedFiltres();
+        }
+
+        private void cmbGender_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SortedGender();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+          
+            if(tbFiltres.Text != "")
+            {
+                dg.ItemsSource = DataBase.tbE.Users.Where(x => x.Surname.Contains(tbFiltres.Text)|| x.Name.Contains(tbFiltres.Text)).ToList();
+            }
+          
+          
+         
+        }
+    }
+}
